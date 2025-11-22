78eb4873414f717e4264e6b7e59ce5d1cf9aa53a
taiwoj14 <129912914+taiwoj14@users.noreply.github.com>
2025-11-21 22:53:00 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "container_name" {
 variable "image" {
   description = "The image URI"
   type        = string
-  default     = "ooghenekaro/myflaskapp:10"
+  default     = "josepht05/petclinic-1:10"
 }
 
 variable "memory" {
