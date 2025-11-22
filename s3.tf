0fa8aa756f6abd04bff956d2736370b44d2876ad
taiwoj14 <129912914+taiwoj14@users.noreply.github.com>
2025-11-21 21:02:26 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # AWS S3 Bucket for AppSpec file
 resource "aws_s3_bucket" "appspec_bucket" {
-  bucket = "karo-appspec-bucket" 
+  bucket = "tai-appspec-bucket" 
 
   tags = {
     Name = "AppSpecBucket"
