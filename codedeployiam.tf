3c504b7e4fcaa0e62840eeb5bd49749cd54f14ac
taiwoj14 <129912914+taiwoj14@users.noreply.github.com>
2025-11-29 20:40:55 -0500

    Update codedeployiam.tf

--- codedeployiam.tf
+++ codedeployiam.tf
@@ -71,7 +71,7 @@ data "aws_iam_policy_document" "codedeploy_role_policy" {
 
 
 resource "aws_iam_role" "codedeploy_role" {
-  name               = "bgcodepb_codedeploy_role"
+  name               = "bgcodepb_codedeploy_role_b"
   assume_role_policy = data.aws_iam_policy_document.codedeploy_assume_policy.json
 }
 
